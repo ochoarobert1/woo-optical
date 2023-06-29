c7a1c76118f405d43cd0510ea86169fdab306c8b
Robert Ochoa <ochoa.robert1@gmail.com>
2023-06-28 23:12:41 -0400

    Estilos de prueba

--- admin/partials/woo-optical-admin-display.php
+++ admin/partials/woo-optical-admin-display.php
@@ -11,3 +11,24 @@
  * @package    Woo_Optical
  * @subpackage Woo_Optical/admin/partials
  */
+
+if (!current_user_can('manage_options')) {
+	return;
+}
+
+if (isset($_GET['settings-updated'])) {
+	add_settings_error('woo_optical_messages', 'woo_optical_message', __('Settings Saved', 'woo-optical'), 'updated');
+}
+
+settings_errors('woo_optical_messages');
+?>
+<div class="wrap">
+	<h1><?php echo esc_html(get_admin_page_title()); ?></h1>
+	<form action="options.php" method="post">
+		<?php
+		settings_fields('woo_optical_prices');
+		do_settings_sections('woo_optical_prices');
+		submit_button(__('Save Settings', 'woo-optical'));
+		?>
+	</form>
+</div>
